4ae6398e133e6dd9ce29d558507b2e8e25bc4fbe
Austin Dang <austindang415@gmail.com>
2022-05-24 15:06:29 -0700

    typo

--- app/models/group.py
+++ app/models/group.py
@@ -5,6 +5,8 @@ class Group(db.Model):
   __tablename__ = "groups"
 
   id = db.Column(db.Integer, primary_key=True)
+  messages = db.realtion
+
   hidden = db.Column(db.Boolean, nullable=False)
 
-  users = db.relationships("User", back_populates='subscribed_groups', secondary=gs)
+  users = db.relationship("User", back_populates='subscribed_groups', secondary=gs)
