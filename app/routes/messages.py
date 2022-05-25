5c0b7f7099819960fc3ef6b13515943c268da85f
Downster <b.downing.code@gmail.com>
2022-05-25 15:39:41 -0700

    Testing

--- app/routes/messages.py
+++ app/routes/messages.py
@@ -1,5 +1,12 @@
+from flask import Blueprint
+message_routes = Blueprint('messages', __name__)
+
 
 def message():
   pass
 
+<<<<<<< HEAD
 auth_routes = Blueprint('auth', __name__)
+=======
+
+>>>>>>> 49a80d1 (Testing)
