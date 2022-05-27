123e51e3d7b659da01d3872ca6c87f5505e12f9f
Austin Dang <austindang415@gmail.com>
2022-05-26 17:11:29 -0700

    Demo User

--- react-app/src/components/auth/DemoUser/DemoUser.js
+++ react-app/src/components/auth/DemoUser/DemoUser.js
@@ -1,3 +1,20 @@
 import React from 'react';
 import { useDispatch} from 'react-redux';
+import * as sessionActions from '../../../store/session'
+import './DemoUser.css'
 
+function DemoUser() {
+   const dispatch = useDispatch();
+
+   const handleSubmit = e => {
+      e.preventDefault();
+      return dispatch(sessionActions.demoUser({credential: 'demo@aa.io', password: 'password'}))
+   }
+   return (
+      <div>
+         <button className='demo-button' onClick={handleSubmit}>Demo User</button>
+      </div>
+   )
+}
+
+export default DemoUser
