b7950e92a7290f8a3b2be2892d9506cbb5fe7778
Celeste Winterton <celestewinterton@gmail.com>
2022-05-28 16:03:18 -0700

    filtered message threads for current user

--- react-app/src/components/Groups/GroupCard/index.js
+++ react-app/src/components/Groups/GroupCard/index.js
@@ -1,11 +1,15 @@
+import { useSelector } from "react-redux";
 
 
 const GroupCard = ({ group }) => {
+    const currentUser = useSelector(state => state.session.user)
     const users = group.users.filter(user => user.username)
+    const usernames = users.map(user => user.username)
+    const filtered = usernames.includes(currentUser.username)
 
     return (
         <div>
-            <div>{users.length ? users.map((user, i) => user.username + ((users.length - 1 == i) ? "" : ", ")) : ""}</div>
+            <div>{filtered ? users[0].image + usernames.join(", ") : null}</div>
         </div>
     )
 }
