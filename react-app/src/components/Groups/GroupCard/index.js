40854eb5a01f5e04462b7cb12a00c2fe20867f58
Celeste Winterton <celestewinterton@gmail.com>
2022-05-28 15:11:36 -0700

    groups card showing comma-sepparated names

--- react-app/src/components/Groups/GroupCard/index.js
+++ react-app/src/components/Groups/GroupCard/index.js
@@ -2,8 +2,11 @@
 
 const GroupCard = ({ group }) => {
     const users = group.users.filter(user => user.username)
+
     return (
-        <h1>poop</h1>
+        <div>
+            <div>{users.length ? users.map((user, i) => user.username + ((users.length - 1 == i) ? "" : ", ")) : ""}</div>
+        </div>
     )
 }
 
