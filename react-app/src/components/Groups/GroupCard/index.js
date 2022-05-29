c7f3ee6fe473bed74b4b36f264d9612bdf8227ee
Celeste Winterton <celestewinterton@gmail.com>
2022-05-29 12:36:48 -0700

    moves some things around in groups to match channels logic

--- react-app/src/components/Groups/GroupCard/index.js
+++ react-app/src/components/Groups/GroupCard/index.js
@@ -16,17 +16,23 @@ const GroupCard = ({ group, all, single, modal, nav }) => {
     const groupName = usernames?.filter(user => user != sessionUser.username).join(", ")
     const history = useHistory()
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     const users = group.users.filter(user => user.username)
 >>>>>>> dfbdf86 (fixing merge conflicts)
+=======
+>>>>>>> befa05e (moves some things around in groups to match channels logic)
     const [showDeleteModal, setShowDeleteModal] = useState(false);
     const [showModal, setShowModal] = useState(false);
     const dispatch = useDispatch()
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     console.log("GROUPCARD ===> ", single, all)
 >>>>>>> dfbdf86 (fixing merge conflicts)
+=======
+>>>>>>> befa05e (moves some things around in groups to match channels logic)
 
     const deleteGroup = () => {
         dispatch(deleteGroupRoom(group.id))
@@ -36,12 +42,17 @@ const GroupCard = ({ group, all, single, modal, nav }) => {
 
     return (
 <<<<<<< HEAD
+<<<<<<< HEAD
         <>
             <div>{filtered && nav ?
 =======
         <div>
             {filtered && nav ?
 >>>>>>> dfbdf86 (fixing merge conflicts)
+=======
+        <>
+            <div>{filtered && nav ?
+>>>>>>> befa05e (moves some things around in groups to match channels logic)
             <NavLink className="groups-nav" to={`/groups/${group.id}`}>
                 <img className="side-nav-img" src="https://user-images.githubusercontent.com/96894806/170845227-028c8ef0-17a6-4b92-a334-038e4f6a469b.png" />
                 <div>
@@ -70,15 +81,15 @@ const GroupCard = ({ group, all, single, modal, nav }) => {
             </div>
             }
 
-            {showDeleteModal && (
-                <Modal onClose={() => setShowModal(false)}>
-                    <h1>Are you sure you want to delete this chatting room?</h1>
-                    <button onClick={deleteGroup}>Yes</button>
-                    <button onClick={() => setShowDeleteModal(false)}>No</button>
-                </Modal>
-            )}
-
-        </div>
+                {showDeleteModal && (
+                    <Modal onClose={() => setShowModal(false)}>
+                        <h1>Are you sure you want to delete this chatting room?</h1>
+                        <button onClick={deleteGroup}>Yes</button>
+                        <button onClick={() => setShowDeleteModal(false)}>No</button>
+                    </Modal>
+                )}
+            </div>
+        </>
     )
 }
 
