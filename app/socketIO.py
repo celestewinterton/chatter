fe2651a4d0225c6832649b849cdd0d0cffac42cc
Downster <b.downing.code@gmail.com>
2022-05-29 12:07:59 -0700

    lll

--- app/socketIO.py
+++ app/socketIO.py
@@ -1,4 +1,5 @@
 from flask_socketio import emit, join_room, leave_room, send, SocketIO
+from .models import Message, db
 
 
 
@@ -8,8 +9,18 @@ socketio = SocketIO(cors_allowed_origins="*")
 
 @socketio.on("chat")
 def handle_chat(data):
-    print(data)
     room = data['room']
+    params = {
+        'owner_id': int(data['userId']),
+        'body': data['msg']
+    }
+    if room[0] == 'c':
+        params['channel_id'] = int(room[1:])
+    else:
+        params['group_id'] = int(room[1:])
+    message = Message(**params)
+    db.session.add(message)
+    db.session.commit()
     emit("chat", data, broadcast=True, to=room)
 
 
