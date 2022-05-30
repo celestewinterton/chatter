a412cab25fd90c92464f7b88ab76ac11e0e12051
Celeste Winterton <celestewinterton@gmail.com>
2022-05-29 17:02:29 -0700

    user search on navbar working

--- react-app/src/components/Navigation/Search.js
+++ react-app/src/components/Navigation/Search.js
@@ -1,12 +1,19 @@
-import React from "react";
-import { useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
 import { ReactSearchAutocomplete } from "react-search-autocomplete";
+import { loadUsers } from "../../store/users";
 
 
 function Search() {
+  const dispatch = useDispatch()
+  const channels = useSelector(state => state.channels.all)
   const users = useSelector(state => state.users)
-  const usersArr = Object.values(users)
+  const usersArray = Object.values(users)
+  const items = Object.values(channels).concat(Object.values(users))
 
+  useEffect(() => {
+    dispatch(loadUsers())
+  }, [dispatch]);
 
   const handleOnSelect = (item) => {
     console.log(item, item.id);
@@ -20,6 +27,8 @@ function Search() {
         <img className='result-image' src={item?.image ? item.image : null} alt=''></img>
         <span className="result-span">id: {item.id}</span>
         <span className="result-span">username: {item.username}</span>
+        {/* <span className="result-span">name: {item.name}</span> */}
+
       </div>
     );
   };
@@ -27,18 +36,18 @@ function Search() {
   return (
     <div className="search-container">
       <ReactSearchAutocomplete
-        items={usersArr}
-        fuseOptions={{ keys: ["username"] }} // Search on both fields
+        items={items}
+        fuseOptions={{ keys: ["username", "name"] }} // Search on both fields
         resultStringKeyName="username" // String to display in the results
         showIcon={true}
         placeholder={'Search for channels or users'}
         onSelect={handleOnSelect}
         styling={{
-          // margin: "8px 0px",
           height: "26px",
           borderRadius: "6px",
           border: "1px solid #504e52",
           backgroundColor: "#d1d2d333",
+          color: "#d1d2d3",
           boxShadow: "none",
           hoverBackgroundColor: "steelblue",
           clearIconMargin: "3px 8px 0 0",
