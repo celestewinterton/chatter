053e1066b6c9efeb7cd003bca7482f234e0eab6f
Austin Dang <austindang415@gmail.com>
2022-05-29 19:54:39 -0700

    favicon, logo, signin and signup buttons

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -33,6 +33,7 @@ function ProfileButton({ user }) {
   return (
     <div className="profile-and-dropdown">
       <div className='profile-container'>
+        {user.username}
         <img className="user-image-nav-1"
           src={user.photo}
           onClick={openMenu}
