68a89b13550e0534ccf9b215a1da1bd6a4858f8b
Downster <b.downing.code@gmail.com>
2022-05-31 11:30:18 -0700

    Fixed weird stuff

--- react-app/src/components/Groups/GroupCard/index.js
+++ react-app/src/components/Groups/GroupCard/index.js
@@ -18,32 +18,10 @@ const GroupCard = ({ group, all, single, modal, nav }) => {
     const filtered = usernames?.includes(sessionUser?.username)
     const groupName = usernames?.filter(user => user != sessionUser.username).join(", ")
     const history = useHistory()
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    const users = group.users.filter(user => user.username)
->>>>>>> dfbdf86 (fixing merge conflicts)
-=======
->>>>>>> befa05e (moves some things around in groups to match channels logic)
     const [showDeleteModal, setShowDeleteModal] = useState(false);
     const [showModal, setShowModal] = useState(false);
     const dispatch = useDispatch()
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    console.log("GROUPCARD ===> ", single, all)
->>>>>>> dfbdf86 (fixing merge conflicts)
-=======
->>>>>>> befa05e (moves some things around in groups to match channels logic)
-=======
-    console.log("==========> ",  singleGroupName, groupId)
->>>>>>> 1108619 (groups title showing on single group page again)
-=======
-    console.log("==========> ",  singleGroupName, singleGroupId, groupId)
->>>>>>> b4c041d (updated model for group cascade delte)
+    console.log("==========> ", singleGroupName, singleGroupId, groupId)
 
     const deleteGroup = () => {
         dispatch(deleteGroupRoom(group.id))
@@ -53,57 +31,24 @@ const GroupCard = ({ group, all, single, modal, nav }) => {
     }
 
     return (
-<<<<<<< HEAD
-<<<<<<< HEAD
         <>
             <div>{filtered && nav ?
-<<<<<<< HEAD
-=======
-        <div>
-            {filtered && nav ?
->>>>>>> dfbdf86 (fixing merge conflicts)
-=======
-        <>
-            <div>{filtered && nav ?
->>>>>>> befa05e (moves some things around in groups to match channels logic)
-            <NavLink className="groups-nav" to={`/groups/${group.id}`}>
-                <img className="side-nav-img" src="https://user-images.githubusercontent.com/96894806/170845227-028c8ef0-17a6-4b92-a334-038e4f6a469b.png" />
-                <div>
-=======
-            <NavLink className="groups-nav grey-hover" to={`/groups/${group.id}`}>
-                <div className="groups-nav-name">
-                    <img className="side-nav-img" src="https://user-images.githubusercontent.com/96894806/170845227-028c8ef0-17a6-4b92-a334-038e4f6a469b.png" />
->>>>>>> 42eb557 (fixing up left navbar)
-                    <div>{groupName}</div>
-                </div>
-                <div>
-                    <i className="fas fa-xmark" onClick={() => setShowDeleteModal(true)}></i>
-                </div>
-            </NavLink> : null}
+                <NavLink className="groups-nav grey-hover" to={`/groups/${group.id}`}>
+                    <div className="groups-nav-name">
+                        <img className="side-nav-img" src="https://user-images.githubusercontent.com/96894806/170845227-028c8ef0-17a6-4b92-a334-038e4f6a469b.png" />
+                        <div>{groupName}</div>
+                    </div>
+                    <div>
+                        <i className="fas fa-xmark" onClick={() => setShowDeleteModal(true)}></i>
+                    </div>
+                </NavLink> : null}
 
 
-            {single &&
-            <div className="groups-header">
-<<<<<<< HEAD
-                <h1 className="groups-title" onClick={modal}>{groupName}</h1>
-<<<<<<< HEAD
-            </div>
-            }
-
-                {showDeleteModal && (
-                    <Modal onClose={() => setShowModal(false)}>
-                        <h1>Are you sure you want to delete this chatting room?</h1>
-                        <button onClick={deleteGroup}>Yes</button>
-                        <button onClick={() => setShowDeleteModal(false)}>No</button>
-                    </Modal>
-                )}
-=======
->>>>>>> dfbdf86 (fixing merge conflicts)
-=======
-                <h1 className="groups-title" onClick={modal}>{singleGroupName.filter(user => user != sessionUser.username).join(", ")}</h1>
->>>>>>> 1108619 (groups title showing on single group page again)
-            </div>
-            }
+                {single &&
+                    <div className="groups-header">
+                        <h1 className="groups-title" onClick={modal}>{singleGroupName.filter(user => user != sessionUser.username).join(", ")}</h1>
+                    </div>
+                }
 
                 {showDeleteModal && (
                     <Modal onClose={() => setShowModal(false)}>
