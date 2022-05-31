98e8b6c620863f7c54e2ff592643b775759b884b
Jingling Jin <jljin0524@gmail.com>
2022-05-31 11:40:31 -0500

    working on edit group

--- react-app/src/components/Groups/GroupsPage/index.js
+++ react-app/src/components/Groups/GroupsPage/index.js
@@ -1,16 +1,24 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getGroupRooms } from "../../../store/chatRooms";
 import { useParams } from 'react-router-dom';
+import { Modal } from '../../../context/Modal'
+import EditGroupForm from "../EditGroupForm"
 
-
-const GroupsPage = ({ single }) => {
+const GroupsPage = ({ groupName }) => {
     const dispatch = useDispatch()
     const { groupId } = useParams()
+
     const user = useSelector(state => state.session.user)
     const groups = useSelector(state => state.chatRooms.subscribed)
+
     const group = Object.values(groups).find(group => group.id == groupId)
     // const members = group.users.map(user => user.username)?.join(", ")
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        return () => setShowModal(false);
+    }, []);
 
     useEffect(() => {
         dispatch(getGroupRooms())
@@ -20,6 +28,15 @@ const GroupsPage = ({ single }) => {
         <div className="app-body">
             <div className="groups-header">
                 <h1 className="groups-title">members</h1>
+                <h1 className="groups-title">{groupName}</h1>
+                <p>Add People
+                   <i className="fas fa-plus" onClick={() => setShowModal(true)}></i>
+                </p>
+                {showModal && (
+                    <Modal onClose={() => setShowModal(false)}>
+                        <EditGroupForm setShowModal={setShowModal} />
+                    </Modal>
+                )}
             </div>
         </div>
     )
