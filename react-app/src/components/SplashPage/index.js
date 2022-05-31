fddcdebcd7883aa9461bee82879d905c3219950b
Austin Dang <austindang415@gmail.com>
2022-05-31 15:50:37 -0700

    css

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -24,8 +24,8 @@ const SplashPage = () => {
                <h1 className='splash-container-1-header'>Chatter is your digital HQ </h1>
                <p className='splash-container-1-text'>Transform the way you work with one place for everyone and everything you need to get stuff done.</p>
                <div className='splash-buttons'>
-                  <DemoUser />
                   <SignUpFormModal />
+                  <DemoUser />
                </div>
             </div>
             <div className='splash-section-1-image'>
