a91fc06a665b49d70bf3926df0d10564edff7acb
Jingling Jin <jljin0524@gmail.com>
2022-06-01 11:27:23 -0500

    add ggroups edit routes

--- app/api/group_routes.py
+++ app/api/group_routes.py
@@ -65,14 +65,38 @@ def create_group():
             db.session.commit()
             return new_group.to_dict()
     return {'errors': form_validation_errors(form.errors)}, 401
-#
-
-# delete group/hide group??
-
 
 @group_routes.route("/<int:groupId>", methods=["PUT"])
-def hide_group(groupId):
-    pass
+def edit_group(groupId):
+    form = NewGroupForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    members = form.data["members"]
+    members_list = members.split(',')
+    strippedMembers = [int(member.strip()) for member in members_list]
+
+    def validate_group(input):
+        groups = Group.query.all()
+        groups_as_dicts = [group.compare_dict() for group in groups]
+        for group in groups_as_dicts:
+            print('GROUP     ', group['user_id'] == input)
+            print('INPUT      ', input)
+            if group['user_id'] == input:
+                return True
+
+    if form.validate_on_submit():
+        if validate_group(strippedMembers):
+            return {'errors': 'group already exists'}, 401
+        else:
+            edit_group = Group.query.get(groupId)
+            for member in strippedMembers:
+                user = User.query.filter(User.id == member).first()
+                edit_group.users.append(user)
+
+            db.session.add(edit_group)
+            db.session.commit()
+            return edit_group.to_dict()
+    return {'errors': form_validation_errors(form.errors)}, 401
+
 
 @group_routes.route("/<int:groupId>", methods=["DELETE"])
 def delete_group(groupId):
