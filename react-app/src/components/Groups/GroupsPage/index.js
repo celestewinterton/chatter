52a9a163b9aac6b34f51da6c0c56e4a07ce2c679
Jingling Jin <jljin0524@gmail.com>
2022-06-01 11:12:16 -0500

    aedit GrouCard and GroupsPage and add EditGroupForm components

--- react-app/src/components/Groups/GroupsPage/index.js
+++ react-app/src/components/Groups/GroupsPage/index.js
@@ -1,16 +1,22 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getGroupRooms } from "../../../store/chatRooms";
 import { useParams } from 'react-router-dom';
+import { Modal } from '../../../context/Modal'
+import EditGroupForm from "../EditGroupForm"
 
-
-const GroupsPage = ({ single }) => {
+const GroupsPage = () => {
     const dispatch = useDispatch()
-    const { groupId } = useParams()
-    const user = useSelector(state => state.session.user)
     const groups = useSelector(state => state.chatRooms.subscribed)
-    const group = Object.values(groups).find(group => group.id == groupId)
-    // const members = group.users.map(user => user.username)?.join(", ")
+    const params = useParams()
+    const singleGroupId = params.id
+    const singleGroupName = Object.values(groups)?.find(group => group.id == singleGroupId)?.users?.map(user => user?.username)
+    const sessionUser = useSelector(state => state.session.user)
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        return () => setShowModal(false);
+    }, []);
 
     useEffect(() => {
         dispatch(getGroupRooms())
@@ -19,11 +25,17 @@ const GroupsPage = ({ single }) => {
     return (
         <div className="app-body">
             <div className="groups-header">
-                <h1 className="groups-title">members</h1>
+                <h1 className="groups-title" >{singleGroupName.filter(user => user != sessionUser.username).join(", ")}</h1>
+                <i className="fas fa-plus" onClick={() => setShowModal(true)}></i>
+
+                {showModal && (
+                    <Modal onClose={() => setShowModal(false)}>
+                        <EditGroupForm setShowModal={setShowModal} />
+                    </Modal>
+                )}
             </div>
         </div>
     )
 }
-
 
 export default GroupsPage
