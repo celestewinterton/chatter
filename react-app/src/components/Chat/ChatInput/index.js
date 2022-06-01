d93bc9fe76304a7676b966ca8f782fe6d7be9f1d
Downster <b.downing.code@gmail.com>
2022-06-01 13:30:28 -0700

    Chat styled

--- react-app/src/components/Chat/ChatInput/index.js
+++ react-app/src/components/Chat/ChatInput/index.js
@@ -1,11 +1,20 @@
 import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css';
+
 
 const ChatInput = ({ value, onChange, send }) => {
+
+    const toolbarOptions = [['bold', 'italic', 'strike'], ['link'], [{ 'list': 'ordered' }, { 'list': 'bullet' }], ['blockquote'], ['code', 'codeblock']];
+    const modules = {
+        'toolbar': toolbarOptions,
+    }
 
     return (
         <>
             <div className='chat-input'>
                 <ReactQuill value={value}
+                    modules={modules}
+                    theme='snow'
                     onChange={onChange} />
                 <button onClick={send}>Send</button>
             </div>
