8b0e75cd594977e6ffd967911201b734efc66649
Celeste Winterton <celestewinterton@gmail.com>
2022-06-01 11:12:20 -0700

    added dropdowns for left menu

--- react-app/src/components/LeftMenu/index.js
+++ react-app/src/components/LeftMenu/index.js
@@ -5,7 +5,11 @@ import Groups from "../Groups"
 import { NavLink } from "react-router-dom"
 import "./LeftMenu.css"
 import { useState, useEffect } from "react"
+<<<<<<< HEAD
 import { DarkModal } from '../../context/Modal'
+=======
+import { Modal } from '../../context/Modal'
+>>>>>>> 77ae731 (added dropdowns for left menu)
 import ChannelForm from "../Channels/ChannelForm";
 
 
@@ -29,6 +33,7 @@ const LeftMenu = () => {
                 <div className="channel-container">
                     <div className="left-menu-label">
                         <button className='left-menu-button unset' onClick={e => showChannels ? setShowChannels(false) : setShowChannels(true)}>
+<<<<<<< HEAD
                             <i className="fa-solid fa-caret-down" style={showChannels ? null : {transform: "rotate(270deg)"}}></i>Channels
                         </button>
                         <i className="fas fa-plus" onClick={() => setShowModal(true)}></i>
@@ -36,6 +41,15 @@ const LeftMenu = () => {
                             <DarkModal onClose={() => setShowModal(false)}>
                                 <ChannelForm setShowModal={setShowModal} />
                             </DarkModal>
+=======
+                            <i class="fa-solid fa-caret-down" style={showChannels ? null : {transform: "rotate(270deg)"}}></i>Channels
+                        </button>
+                        <i className="fas fa-plus" onClick={() => setShowModal(true)}></i>
+                        {showModal && (
+                            <Modal onClose={() => setShowModal(false)}>
+                                <ChannelForm setShowModal={setShowModal} />
+                            </Modal>
+>>>>>>> 77ae731 (added dropdowns for left menu)
                         )}
                     </div>
                     {showChannels && <Channels user={true} />}
@@ -44,7 +58,11 @@ const LeftMenu = () => {
                     {/* <CreateGroupModal /> */}
                     <div className="left-menu-label">
                         <button className='left-menu-button unset' onClick={e => showGroups ? setShowGroups(false) : setShowGroups(true)}>
+<<<<<<< HEAD
                             <i className="fa-solid fa-caret-down" style={showGroups ? null : {transform: "rotate(270deg)"}}></i>Direct Messages
+=======
+                            <i class="fa-solid fa-caret-down" style={showGroups ? null : {transform: "rotate(270deg)"}}></i>Direct Messages
+>>>>>>> 77ae731 (added dropdowns for left menu)
                         </button>
                         <NavLink className='unset' to={`/groups/new`}><i className="fas fa-plus"></i></NavLink>
                     </div>
