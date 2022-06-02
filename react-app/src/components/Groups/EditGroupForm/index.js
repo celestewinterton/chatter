6946716933389d1c01fa5efb6bed98c5d2ea2516
Jingling Jin <jljin0524@gmail.com>
2022-06-02 11:48:15 -0500

    update CSS in EditGroupForm and GroupsPage

--- react-app/src/components/Groups/EditGroupForm/index.js
+++ react-app/src/components/Groups/EditGroupForm/index.js
@@ -4,7 +4,6 @@ import { createGroupRoom, editGroupRoom } from "../../../store/chatRooms";
 import { loadUsers } from "../../../store/users";
 import SearchAutocomplete from "./Autocomplete";
 import { useParams } from 'react-router-dom';
-import './EditGroupForm.css'
 
 const EditGroupForm = ({ setShowModal, edit, group }) => {
     edit = true
@@ -57,20 +56,22 @@ const EditGroupForm = ({ setShowModal, edit, group }) => {
 
     return (
         <form autoComplete="off" className="edit-group-form-container" onSubmit={handleSubmit}>
-            <h1 className="groups-title" >{singleGroupName.filter(user => user != sessionUser.username).join(", ")}</h1>
+            <h1 className="edit-groups-title" >{singleGroupName.filter(user => user != sessionUser.username).join(", ")}</h1>
             <div className='channel-form-input-container'>
                 <SearchAutocomplete members={members} setMembers={setMembers} />
             </div>
-            <div className="buttons-right-container">
+            <div className="buttons-center-container">
                 <button className='cancel-btn grey-button' onClick={handleCancelClick}>Cancel</button>
                 <button disabled={Object.keys(errors).length > 0} className='green-button' type="submit">{(edit) ? 'Add People' : 'Start DM'}</button>
             </div>
 
-            <ul>
+            <ul >
                 {singleGroupName.map((user,idx) => (
-                    <li key={idx}>
-                        <img className="side-nav-img" src="https://user-images.githubusercontent.com/96894806/170845227-028c8ef0-17a6-4b92-a334-038e4f6a469b.png" />
-                        {user}
+                    <li key={idx} >
+                        <div className="edit-group-from-users">
+                            <img className="side-nav-img" src="https://user-images.githubusercontent.com/96894806/170845227-028c8ef0-17a6-4b92-a334-038e4f6a469b.png" />
+                            <div>{user}</div>
+                        </div>
                     </li>
                 ))}
             </ul>
