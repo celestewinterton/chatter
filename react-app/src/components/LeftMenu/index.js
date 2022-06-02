1b8a865a44f274930ba20f79c274dca2f9ce3b92
Austin Dang <austindang415@gmail.com>
2022-06-02 09:53:24 -0700

    icon

--- react-app/src/components/LeftMenu/index.js
+++ react-app/src/components/LeftMenu/index.js
@@ -25,6 +25,9 @@ const LeftMenu = () => {
                     <NavLink className="unset" to={`/`}>
                         <h1 className="left-menu-title">Chatter</h1>
                     </NavLink>
+                    <div className='left-navigation-icon'>
+                        <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                    </div>
                 </div>
                 <div className="channel-container">
                     <div className="left-menu-label">
