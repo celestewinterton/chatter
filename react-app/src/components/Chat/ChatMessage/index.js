e7d1a135d50fc1e59a5dc1b7a0cf2b309d7a4f28
Downster <b.downing.code@gmail.com>
2022-06-02 15:33:21 -0700

    Fixed edit message

--- react-app/src/components/Chat/ChatMessage/index.js
+++ react-app/src/components/Chat/ChatMessage/index.js
@@ -1,4 +1,4 @@
-import ChatInput from "../ChatInput"
+import EditChatInput from "../ChatInput"
 import Parser from 'html-react-parser';
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
@@ -15,7 +15,6 @@ const ChatMessage = ({ msg, socket, roomId, userId }) => {
 
 
     const editMessage = async (e, msgId) => {
-        e.preventDefault()
         socket.emit('edit', {
             user: `${user.username}`, userId: `${user.id}`, msgId: msgId, msg: message, room: roomId, created_at: (new Date()).toLocaleTimeString()
         });
@@ -35,10 +34,16 @@ const ChatMessage = ({ msg, socket, roomId, userId }) => {
         setEdit(true)
     }
 
+    const cancelEdit = () => {
+        dispatch(setEditTrue())
+        setEdit(false)
+    }
+
 
 
     return (
         <>
+<<<<<<< HEAD
             <div className='pic-container'>
                 <ChatUserCard msg={msg} />
             </div>
@@ -53,6 +58,12 @@ const ChatMessage = ({ msg, socket, roomId, userId }) => {
                 <div className='chat-text' id={msg.id}>
                     {(edit) ? <ChatInput value={message} onChange={(e) => setMessageBody(e)} send={(e) => editMessage(e, msg.id)} /> : Parser(msg.message)}
                 </div>
+=======
+            {(edit) ? <EditChatInput value={message} onChange={(e) => setMessageBody(e)} send={(e) => editMessage(e, msg.id)} /> : Parser(msg.message)}
+            <div className="chat-buttons">
+                {(canEdit && userId == msg.owner_id) ? <button onClick={updateEdit}>Edit</button> : <button onClick={cancelEdit}>Cancel</button>}
+                {(userId == msg.owner_id) ? <button onClick={(e) => deleteMessage(e, msg.id)}>Delete</button> : null}
+>>>>>>> d628605 (Fixed edit message)
             </div>
         </>
     )
---
+++ react-app/src/components/Chat/EditChatInput/index.js
@@ -0,0 +1,49 @@
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css';
+
+
+const EditChatInput = ({ value, onChange, send, group, room }) => {
+    let users;
+    if (group) {
+        users = room.users.map(user => user.username).join(', ')
+    } else {
+        users = room?.name
+    }
+    const toolbarOptions = [['bold', 'italic', 'strike'], ['link'], [{ 'list': 'ordered' }, { 'list': 'bullet' }], ['blockquote']];
+    const modules = {
+        'toolbar': toolbarOptions,
+        keyboard: {
+            bindings: {
+                shift_enter: {
+                    key: 13,
+                    shiftKey: true,
+                    handler: (range, ctx) => {
+                        console.log(range, ctx); // if you want to see the output of the binding
+                    }
+                },
+                enter: {
+                    key: 13,
+                    handler: send
+                }
+            }
+        }
+    }
+
+
+    return (
+        <>
+            <div className='chat-input'>
+                <ReactQuill value={value}
+                    modules={modules}
+                    placeholder={(group) ? "Message " + users : "Message #" + users}
+                    theme='snow'
+                    onChange={onChange} />
+                <div className='button-div'>
+                    <button className='send-button' onClick={send}><i className="fa-solid fa-paper-plane"></i></button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default EditChatInput
